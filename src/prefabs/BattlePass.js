7e879c07a8a4f32f76c34e02455ae8c954b0b9b2
Liam Murray <liammurray404@gmail.com>
2024-04-12 11:57:30 -0700

    Fortnite Battle Pass

---
+++ src/prefabs/BattlePass.js
@@ -0,0 +1,34 @@
+class BattlePass {
+    constructor(scene, x, y, scale=1, num_options) {
+        //handle default args
+        this.scene = scene
+        this.x = x
+        this.y = y
+
+        //make attributes
+        this.width = 450
+        this.height = 750
+
+        //this is a stand in variable until we can pass
+        //an array of the companies to the object
+        this.num = num_options
+
+        //build visual elements
+        scene.add.rectangle(x, y, this.width, this.height, 0x808080);
+
+        this.dataText = scene.add.text(x - 115, this.height / 2 + 50, "Battle Pass", {
+            fontSize: 24 
+        }).setOrigin(0.5)
+        
+        this.b1 = new Button(scene, (() => {this.buy(35, 30)}), this, `Option 1: 35% - $30`, x - 115, this.height / 2 + 110)
+        this.b2 = new Button(scene, (() => {this.buy(25, 27)}), this, `Option 1: 25% - $27`, x - 115, this.height / 2 + 180)
+        this.b3 = new Button(scene, (() => {this.buy(15, 20)}), this, `Option 1: 15% - $20`, x - 115, this.height / 2 + 250)
+        this.b4 = new Button(scene, (() => {this.buy(10, 15)}), this, `Option 1: 10% - $15`, x - 115, this.height / 2 + 320)
+
+    }
+
+    buy(percent, cost) {
+        //add in actual value changes later
+        console.log(`bought ${percent}% better odds for ${cost} USD`)
+    }
+}
