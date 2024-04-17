cf26b4b90b574567ea66294cfb2457c29df6c5af
Lyssa Li <98376314+maozblan@users.noreply.github.com>
2024-04-16 22:48:40 -0700

    multi-tab base functionality

--- style/style.js
+++ style/style.js
@@ -3,13 +3,19 @@ $("#game-screen").append($("canvas")); // phaser sometimes REALLY REALLY want to
 $(document).ready(function() {
   scaleBG();
   $("#game-screen").append($("canvas")); // phaser please stop trying to escape
+  
+  // mock "tab physics"
+  $(".tab").draggable();
+  $(".tab").click(function() {
+    reorganizeTabs(`#${$(this).attr('id')}`);
+  });
 });
 
 $(window).resize(scaleBG);
 
 function scaleBG() {
   const aspectRatio = window.innerWidth / window.innerHeight;
-  console.log(aspectRatio, backgroundRatio);
+  // console.log(aspectRatio, backgroundRatio);
   if (aspectRatio < backgroundRatio) {
     $("#desktop-background").css("height", "100vh");
     $("#desktop-background").css("width", "auto");
@@ -18,4 +24,13 @@ function scaleBG() {
     $("#desktop-background").css("width", "100vw");
   }
   $("#game-screen").append($("canvas")); // just incase phaser wants to escape
+}
+
+let openTabs = ['#game-screen', '#bank-screen'];
+function reorganizeTabs(topTab) {
+  openTabs.splice(openTabs.indexOf(topTab), 1);
+  openTabs.unshift(topTab);
+  for (let i = 1; i <= openTabs.length; ++i) {
+    $(openTabs[openTabs.length-i]).css('z-index', i);
+  }
 }
